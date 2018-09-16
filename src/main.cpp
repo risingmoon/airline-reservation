4d836abff0e07793b92e38da08ac7c1674945aba
Justin Lee <j.lee@pokemon.com>
2018-09-16 16:36:49 -0700

    Add user input while

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Airline Reservation" << endl;
+    int choice = 1;
+    while (choice) {
+        cout << "Airline Reservation" << endl;
 
-    // reserve a seat
-    cout << "reserve a seat" << endl;
+        // reserve a seat
+        cout << "reserve a seat" << endl;
 
-    // flight schedule
-    cout << "display flight schedule" << endl;
+        // flight schedule
+        cout << "display flight schedule" << endl;
 
-    // display passenger information
-    cout << "display passenger information" << endl;
+        // display passenger information
+        cout << "display passenger information" << endl;
 
-    // flight details
-    cout << "display flight details" << endl;
+        // flight details
+        cout << "display flight details" << endl;
 
-    // user ticket information
-    cout << "display ticket information" << endl;
+        // user ticket information
+        cout << "display ticket information" << endl;
 
-    // exit the program
-    cout << "exit" << endl;
+        // exit the program
+        cout << "exit" << endl;
+
+       // user input
+        cin >> choice;
+    }
+    cout << "Exiting..." <<  endl;
 
     return 0;
 }
