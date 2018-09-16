70152ad76555c7bb3752951677dbb089c26684bb
Justin Lee <j.lee@pokemon.com>
2018-09-16 16:31:19 -0700

    Add menu options

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,24 @@ using namespace std;
 int main()
 {
     cout << "Airline Reservation" << endl;
+
     // reserve a seat
+    cout << "reserve a seat" << endl;
+
     // flight schedule
+    cout << "display flight schedule" << endl;
+
     // display passenger information
+    cout << "display passenger information" << endl;
+
     // flight details
+    cout << "display flight details" << endl;
+
     // user ticket information
+    cout << "display ticket information" << endl;
+
     // exit the program
+    cout << "exit" << endl;
+
     return 0;
 }
