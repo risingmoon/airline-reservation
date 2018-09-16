530f724779d2e29d62e3ff03abc3e0f4313f423b
Justin Lee <j.lee@pokemon.com>
2018-09-16 16:11:56 -0700

    Add annotations

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
     cout << "Airline Reservation" << endl;
+    // reserve a seat
+    // flight schedule
+    // display passenger information
+    // flight details
+    // user ticket information
+    // exit the program
     return 0;
 }
